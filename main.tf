83d9851dcb3374f8d3f2e1cace24124d64039e4a
hacwa <admin@hacwa.co.uk>
2022-12-11 23:00:43 +0000

    2022-12-11-23:00:43

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
     region = "eu-west-2"
-#    access_key = var.AWS_ACCESS_KEY_ID
-#    secret_key = var.AWS_ACCESS_SECRET_ID
     access_key = var.aws_access_key_id
     secret_key = var.aws_access_secret_id
 }
